b940259f3aa9c30e367aea90b141f1173288223b
Shane McDonough <shanemcdman@gmail.com>
2020-06-03 01:06:42 -0700

    Make randomize_heights a function

--- sketch.js
+++ sketch.js
@@ -1,5 +1,9 @@
-var elements = document.getElementsByClassName("inner_item");
-for (item of elements) {
-    random_num = Math.floor(Math.random() * 100)
-    item.style.height = random_num + "%"
+function randomize_heights(){
+    var elements = document.getElementsByClassName("inner_item");
+    for (item of elements) {
+        random_num = Math.floor(Math.random() * 100)
+        item.style.height = random_num + "%"
+    }
 }
+
+randomize();
