122bf4f9166aea5ddde3a5a2ff10f539174b1622
Shane McDonough <shanemcdman@gmail.com>
2020-06-03 01:04:54 -0700

    Start work on js

---
+++ sketch.js
@@ -0,0 +1,5 @@
+var elements = document.getElementsByClassName("inner_item");
+for (item of elements) {
+    random_num = Math.floor(Math.random() * 100)
+    item.style.height = random_num + "%"
+}
