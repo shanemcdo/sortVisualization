8fca56d47d2b24d33392513103183402281fa3ad
Shane McDonough <shanemcdman@gmail.com>
2020-06-03 01:33:00 -0700

    Make it impossible for a width less than 4% to exist

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 function randomize_heights(){
     var elements = document.getElementsByClassName("inner_item");
     for (item of elements) {
-        random_num = Math.floor(Math.random() * 100)
+        random_num = Math.floor(Math.random() * 96) + 4
         item.style.height = random_num + "%"
     }
 }
