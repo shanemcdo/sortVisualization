f313e71dfe9bba034d49d9e2aeb5a1e9b48c3aed
Shane McDonough <shanemcdman@gmail.com>
2020-06-03 19:05:37 -0700

    Create a finished sort for quicksort

--- quick.js
+++ quick.js
@@ -1,10 +1,34 @@
 class QuickSort {
     constructor() {
-        this.speed = 1000 - speed_slider.value
+        // this.speed = 1000 - speed_slider.value
     }
 
-    async sort(arr, low, high){
-        var pivot = Math.floor(Math.random * (high - low)) + low
-        var left = low;
+    async sort(low, high){
+        if(low >= high){
+            return;
+        }
+        var index = this.partition(low, high);
+        this.sort(low, index - 1);
+        this.sort(index + 1, high);
+        update_heights();
+    }
+
+    partition(low, high){
+        var pivot = item_heights[high]
+        var pivot_index = low;
+        for(var i = low; i < high; i++){
+            console.log(item_heights);
+            console.log(pivot_index);
+            if (item_heights[i] < pivot){
+                var temp = item_heights[i];
+                item_heights[i] = item_heights[pivot_index];
+                item_heights[pivot_index] = temp;
+                pivot_index++;
+            }
+        }
+        var temp = item_heights[high];
+        item_heights[high] = item_heights[pivot_index];
+        item_heights[pivot_index] = temp;
+        return pivot_index;
     }
 }
