4bdcf937b6b3e1a825053b395b86bf892b5a521c
Shane McDonough <shanemcdman@gmail.com>
2020-06-04 22:47:48 -0700

    Remove useless code

--- bubble.js
+++ bubble.js
@@ -6,24 +6,6 @@ class BubbleSort {
         this.speed = 1000 - speed_slider.value;
     }
 
-    step() {
-        if(item_heights[this.j - 1] > item_heights[this.j]){
-            var temp = item_heights[this.j - 1];
-            item_heights[this.j - 1] = item_heights[this.j];
-            item_heights[this.j] = temp;
-        }
-        this.j += 1;
-        if(this.j >= item_heights.length - this.i){
-            this.j = 1;
-            this.i += 1;
-            if(this.i >= item_heights.length){
-                this.done = true;
-            }
-        }
-        update_heights();
-        this.highlight();
-    }
-
     highlight(j) {
         reset_coloring();
         var elements = document.getElementsByClassName("inner_item");
